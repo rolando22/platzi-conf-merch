391e05fdded0cc988c5916404af639fccdfa0018
rolando22 <rolando2207@gmail.com>
2022-07-14 18:13:39 -0300

    [UPDATE] Information

--- src/containers/Information/index.js
+++ src/containers/Information/index.js
@@ -4,7 +4,41 @@ import "./Information.css";
 function Information () {
     return (
         <div className="Information">
-            <div className="Information-content"></div>
+            <div className="Information-content">
+                <div className="Information-head">
+                    <h2>Información de contacto:</h2>
+                </div>
+                <div className="Information-form">
+                    <form action="">
+                        <input type="text" placeholder="Nombre completo" name="name" />
+                        <input type="text" placeholder="Correo electrónico" name="email" />
+                        <input type="text" placeholder="Dirección" name="address" />
+                        <input type="text" placeholder="Apto" name="apto" />
+                        <input type="text" placeholder="Ciudad" name="city" />
+                        <input type="text" placeholder="País" name="country" />
+                        <input type="text" placeholder="Estado" name="state" />
+                        <input type="text" placeholder="Código postal" name="cp" />
+                        <input type="text" placeholder="Teléfono" name="phone" />
+                    </form>
+                </div>
+                <div className="Information-buttons">
+                    <div className="Information-back">
+                        Regresar
+                    </div>
+                    <div className="Information-next">
+                        Pagar
+                    </div>
+                </div>
+            </div>
+            <div className="Information-sidebar">
+                <h3>Pedido:</h3>
+                <div className="Information-item">
+                    <div className="Information-element">
+                        <h4>ITEM name</h4>
+                        <span>$10</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
